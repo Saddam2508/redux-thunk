64519688a687ab3ba9ce254a8a6ee92b9d1d8546
Saddam <eyarinsultana@gmail.com>
2025-06-09 22:02:51 +0600

    Setup Redux store with thunk middleware and implemented todos fetching functionality

---
+++ src/services/reducer/todosReducer.js
@@ -0,0 +1,38 @@
+import {
+  GET_TODOS_FAILED,
+  GET_TODOS_REQUEST,
+  GET_TODOS_SUCCESS,
+} from "../constants/todosConstant";
+
+const initialTodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+const todosReducer = (state = initialTodosState, action) => {
+  switch (action.type) {
+    case GET_TODOS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case GET_TODOS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        todos: action.payload,
+      };
+    case GET_TODOS_FAILED:
+      return {
+        isLoading: false,
+        todos: [],
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
